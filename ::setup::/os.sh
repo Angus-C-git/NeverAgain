15c4a65bf585944050fd0037aac1829f7f578549
Gus <anguscornall@gmail.com>
2021-05-19 20:29:28 +1000

    ADDED: shell setup basic plugin alias

--- ::setup::/os.sh
+++ ::setup::/os.sh
@@ -22,7 +22,7 @@ echo "    _|_|_|_|_|                                                            
 echo ""
 echo ""
 
-        # :::::::::::::::::::::::::::::: Terminal Setup ::::::::::::::::::::::::::::::
+     # :::::::::::::::::::::::::::::: Terminal Setup ::::::::::::::::::::::::::::::
 
 echo ""
 echo ""
@@ -35,4 +35,47 @@ sudo apt install -y fish
 sudo apt install -y neofetch
 
 # <- Powerline -> 
+
+pip3 install powerline-shell
+sudo apt-get install -y fonts-powerline
+
+clone
+git clone https://github.com/powerline/fonts.git --depth=1
+# install
+cd fonts
+./install.sh
+# clean-up a bit
+cd ..
+rm -rf fonts
+
+echo "function fish_prompt" >> ~/.config/fish/config.fish
+echo "  powerline-shell --shell bare \$status" >> ~/.config/fish/config.fish
+echo "end" >> ~/.config/fish/config.fish
+echo "" >> ~/.config/fish/config.fish
+# echo "clear" >> ~/.config << Enable this to hide fish welcome
+echo "neofetch" >> ~/.config/fish/config.fish
+
+
+     # :::::::::::::::::::::::::::::: Alias Setup ::::::::::::::::::::::::::::::
+
+
+echo ""
+echo ""
+echo ":::::::::::::::::::::::::::::: [Alias Setup] ::::::::::::::::::::::::::::::"
+echo ""
+echo ""
+
+# <- cat enhancment/plugin -> 
+sudo snap install batcat
+
+# <- ls enhancment/plugin (pain) ->
+sudo apt install -y exa  # << Works on some distros
+# curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+# wget https://github.com/ogham/exa/releases/download/v0.10.0/exa-linux-x86_64-v0.10.0.zip
+# unzip exa-linux-x86_64-v0.10.0.zip
+
+
+# <- Create alias ->
+echo "" >> ~/.config/fish/config.fish
+echo "alias bcat=\"batcat\"" >> ~/.config/fish/config.fish
 
